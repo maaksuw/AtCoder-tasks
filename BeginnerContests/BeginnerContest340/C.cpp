85c26ef47cfe3a3172ca13be856b2ae7b5f3c39f
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2024-02-29 22:44:14 +0200

    Add C exercises.

--- BeginnerContests/BeginnerContest340/C.cpp
+++ BeginnerContests/BeginnerContest340/C.cpp
@@ -1,1 +1,32 @@
 /*Task: https://atcoder.jp/contests/abc340/tasks/abc340_c */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int log2(ll n) {
+    return 63 - __builtin_clzll(n);
+}
+
+ll pow(int n, int e) {
+    ll res = 1;
+    for (int i = 0; i < e; i++) res *= n;
+    return res;
+}
+
+void testcase() {
+    ll n; cin >> n;
+    int exp = log2(n);
+    ll s = pow(2, exp);
+    ll result = exp*n + (n - s)*2;
+    cout << result << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    testcase();
+}
--- BeginnerContests/BeginnerContest342/C.cpp
+++ BeginnerContests/BeginnerContest342/C.cpp
@@ -1,1 +1,34 @@
 /*Task: https://atcoder.jp/contests/abc342/tasks/abc342_c */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+char a [128];
+
+void testcase() {
+    for (int i = 'a'; i <= 'z'; i++) a[i] = i;
+    int n; cin >> n;
+    string s; cin >> s;
+    int q; cin >> q;
+    for (int j = 0; j < q; j++) {
+        char c, d; cin >> c >> d;
+        for (int i = 'a'; i <= 'z'; i++) {
+            if (a[i] == c) a[i] = d;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << a[s[i]];
+    }
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    testcase();
+}
